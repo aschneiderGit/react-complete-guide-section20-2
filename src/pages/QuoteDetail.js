b3759772c5eeca8e10cf898e201656733486a14e
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-28 17:24:56 +0100

    add a button to load components using only path

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,7 +1,8 @@
 import {Fragment} from 'react';
-import {Route, useParams} from 'react-router-dom';
-import Comments from '../components/comments/Comments';
+import {useParams, Route, Link, useRouteMatch} from 'react-router-dom';
+
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
 
 const DUMMY_QUOTES = [
 	{id: 'q1', author: 'Max', text: 'Learning React is fun!'},
@@ -12,15 +13,22 @@ const QuoteDetail = () => {
 	const params = useParams();
 
 	const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
 	if (!quote) {
-		return <p> No quote find !</p>;
+		return <p>No quote found!</p>;
 	}
+
 	return (
 		<Fragment>
 			<HighlightedQuote text={quote.text} author={quote.author} />
-
+			<Route path={`/quotes/${params.quoteId}`} exact>
+				<div className="centered">
+					<Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+						Load Comments
+					</Link>
+				</div>
+			</Route>
 			<Route path={`/quotes/${params.quoteId}/comments`}>
-				{/* or /quotes/:quoteId/comments */}
 				<Comments />
 			</Route>
 		</Fragment>
