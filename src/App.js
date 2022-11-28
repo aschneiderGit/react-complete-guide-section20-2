bd0d9f68d0b999d3d7663ca90e4fa2863f517e79
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-28 16:16:19 +0100

    setup router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div>
+import AllQuotes from './pages/AllQuotes';
+import NewQuote from './pages/NewQuote';
+import QuoteDetail from './pages/QuoteDetail';
 
-    </div>
-  );
+const {Switch, Route} = require('react-router-dom');
+
+function App() {
+	return (
+		<Switch>
+			<Route path="/quotes" exact>
+				<AllQuotes />
+			</Route>
+			<Route path="/quotes/:quoteId">
+				<QuoteDetail />
+			</Route>
+			<Route path="/new-quote">
+				<NewQuote />
+			</Route>
+		</Switch>
+	);
 }
 
 export default App;
